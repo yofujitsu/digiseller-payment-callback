177078cada3dce33249759192c27b09dc2844a9c
yofujitsu <yofujitsuuu@gmail.com>
2025-08-09 15:16:46 +0300

    master: init commit

---
+++ src/main/java/ru/yofujitsu/digiseller_payment_callback/DigisellerPaymentCallbackApplication.java
@@ -0,0 +1,13 @@
+package ru.yofujitsu.digiseller_payment_callback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigisellerPaymentCallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigisellerPaymentCallbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yofujitsu/digiseller_payment_callback/DigisellerPaymentCallbackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yofujitsu.digiseller_payment_callback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigisellerPaymentCallbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
