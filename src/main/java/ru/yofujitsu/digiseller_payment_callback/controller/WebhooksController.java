06b17ca8ec14c8b069108fa0b774533c84245ca2
yofujitsu <yofujitsuuu@gmail.com>
2025-08-14 18:29:49 +0300

    master: added controller logs

--- src/main/java/ru/yofujitsu/digiseller_payment_callback/controller/WebhooksController.java
+++ src/main/java/ru/yofujitsu/digiseller_payment_callback/controller/WebhooksController.java
@@ -1,6 +1,7 @@
 package ru.yofujitsu.digiseller_payment_callback.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yofujitsu.digiseller_payment_callback.dto.WebhookResponseDto;
@@ -8,12 +9,14 @@ import ru.yofujitsu.digiseller_payment_callback.service.WebhooksService;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class WebhooksController {
 
     private final WebhooksService webhooksService;
 
     @PostMapping("/webhook/sale")
     public ResponseEntity<Void> handlePaymentPost(@RequestBody WebhookResponseDto webhookResponseDto) {
+        log.info("Received POST webhook response: {}", webhookResponseDto);
         webhooksService.handlePayment(Long.parseLong(webhookResponseDto.orderId()));
         return ResponseEntity.ok().build();
     }
@@ -30,6 +33,8 @@ public class WebhooksController {
                                                  @RequestParam String agent,
                                                  @RequestParam String cart_uid,
                                                  @RequestParam String isMyProduct) {
+        log.info("Received GET webhook response: {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}", id_i, id_d, amount,
+                curr, date, email, sha256, ip, agent, cart_uid, isMyProduct);
         webhooksService.handlePayment(Long.parseLong(id_i));
         return ResponseEntity.ok().build();
     }
