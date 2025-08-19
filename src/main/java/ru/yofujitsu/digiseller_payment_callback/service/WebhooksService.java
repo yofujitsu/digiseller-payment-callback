9a442fc95f1610a8e0a1b1991c391a51b3acaa0b
yofujitsu <yofujitsuuu@gmail.com>
2025-08-19 21:58:36 +0300

    master: add message logs

--- src/main/java/ru/yofujitsu/digiseller_payment_callback/service/WebhooksService.java
+++ src/main/java/ru/yofujitsu/digiseller_payment_callback/service/WebhooksService.java
@@ -13,6 +13,7 @@ public class WebhooksService {
     private final String MESSAGE = System.getenv("MESSAGE");
 
     public void handlePayment(long orderId, String email, String amount, String curr) {
+        log.info("MESSAGE: {}", MESSAGE);
         log.info("Произошла покупка товара c ID {}, от пользователя с email: {}, на сумму: {} {}", orderId, email, amount, curr);
         digisellerPaymentService.sendMessage(MESSAGE, orderId);
         log.info("Отправил ответное сообщение покупателю.");
